7897ac63e07481cf5f321a1d9183da95a873a44c
HideComet <s321790@gmail.com>
2016-07-21 11:16:01 +0800

    2016

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -4,7 +4,8 @@
 #include <iostream>  
 #include <string> 
 #include <fstream>
-class OR_mnist;
+#include <time.h>
+
 using namespace std;
 using namespace cv;
 
@@ -13,7 +14,7 @@ void writeMatToFile(cv::Mat& m, const char* filename)
 	ofstream fout(filename);
 	for (int i = 0; i<m.rows; i++){
 		for (int j = 0; j<m.cols; j++){
-			fout << m.at<int>(i, j) << "\t";
+			fout << m.at<float>(i, j) << "\t";
 		}
 		fout << endl;
 	}
@@ -21,16 +22,16 @@ void writeMatToFile(cv::Mat& m, const char* filename)
 }
 void determine(int number, Mat data, int number_of_items){
 
-	if (number == 0){ data.at<int>(number_of_items, 0) = 1; }
-	if (number == 1){ data.at<int>(number_of_items, 1) = 1; }
-	if (number == 2){ data.at<int>(number_of_items, 2) = 1; }
-	if (number == 3){ data.at<int>(number_of_items, 3) = 1; }
-	if (number == 4){ data.at<int>(number_of_items, 4) = 1; }
-	if (number == 5){ data.at<int>(number_of_items, 5) = 1; }
-	if (number == 6){ data.at<int>(number_of_items, 6) = 1; }
-	if (number == 7){ data.at<int>(number_of_items, 7) = 1; }
-	if (number == 8){ data.at<int>(number_of_items, 8) = 1; }
-	if (number == 9){ data.at<int>(number_of_items, 9) = 1; }
+	if (number == 0){ data.at<float>(number_of_items, 0) = 1.0; }
+	if (number == 1){ data.at<float>(number_of_items, 1) = 1.0; }
+	if (number == 2){ data.at<float>(number_of_items, 2) = 1.0; }
+	if (number == 3){ data.at<float>(number_of_items, 3) = 1.0; }
+	if (number == 4){ data.at<float>(number_of_items, 4) = 1.0; }
+	if (number == 5){ data.at<float>(number_of_items, 5) = 1.0; }
+	if (number == 6){ data.at<float>(number_of_items, 6) = 1.0; }
+	if (number == 7){ data.at<float>(number_of_items, 7) = 1.0; }
+	if (number == 8){ data.at<float>(number_of_items, 8) = 1.0; }
+	if (number == 9){ data.at<float>(number_of_items, 9) = 1.0; }
 
 }
 
@@ -97,7 +98,7 @@ Mat read_Mnist_Label(string filename)
 		number_of_items = reverseInt(number_of_items);
 		
 		unsigned int s = 0, e = 0;
-		LabelMat = Mat::zeros(number_of_items, 10, CV_32SC1);
+		LabelMat = Mat::zeros(number_of_items, 10, CV_32FC1);
 		for (int i = 0; i < number_of_items; i++) {
 			unsigned char temp = 0;
 			file.read((char*)&temp, sizeof(temp));
@@ -110,8 +111,7 @@ Mat read_Mnist_Label(string filename)
 	return LabelMat;
 }
 
-
-int main(){
+void NeuralNetwork(Mat layerSizes){
 	CvANN_MLP bp;
 	CvANN_MLP*nnetwork;
 	CvANN_MLP_TrainParams params;
@@ -119,24 +119,60 @@ int main(){
 	params.bp_dw_scale = 0.1;
 	params.bp_moment_scale = 0.1;
 
-	/*float labels[10][2] = {{0.9,0.1}, {0.9,0.1}, {0.9,0.1}, {0.9,0.1}, {0.9,0.1}, {0.1,0.9}, {0.1,0.9}, {0.1,0.9}, {0.1,0.9}, {0.1,0.9}};
-	Mat labelsMat(10, 2, CV_32FC1, labels);*/
-	/*float trainingData[10][2] = { { 100, 100 }, { 20, 20 }, { 30, 30 }, { 40, 40 }, { 50, 50 }, { 200, 50 }, { 20, 200 }, { 300, 300 }, { 400, 400 }, {500,500}};
-	Mat trainingDataMat(10, 2, CV_32FC1, trainingData);*/
-	
-	Mat mnist_image_data = read_mnist_image("MNIST/t10k-images.idx3-ubyte");
+	Mat mnist_image_data = read_mnist_image("MNIST/train-images.idx3-ubyte");
+	cout << mnist_image_data.rows << " :" << mnist_image_data.cols << endl;
+	Mat mnist_label_data = read_Mnist_Label("MNIST/train-labels.idx1-ubyte");
+	cout << mnist_label_data.rows << " :" << mnist_label_data.cols << endl;
+
+	//writeMatToFile(mnist_image_data, "mnist_image_data.txt");
+	//writeMatToFile(mnist_label_data, "mnist_label_data.txt");
+
+	bp.create(layerSizes, CvANN_MLP::SIGMOID_SYM);
+	bp.train(mnist_image_data, mnist_label_data, Mat(), Mat(), params);
+	bp.save("bp.xml");
+}
+int main(){
+/*	time_t nStart = time(NULL);
+	Mat layerSizes = (Mat_<int>(1, 3) << 784, 300, 10);
+	NeuralNetwork(layerSizes);
+	time_t nEnd = time(NULL);
+	cout << nEnd - nStart << "總訓練秒數" << endl;*/
+
+/*	Mat mnist_image_data = read_mnist_image("MNIST/train-images.idx3-ubyte");
 	cout << mnist_image_data.rows << " :" << mnist_image_data.cols << endl;
 	
-	Mat mnist_label_data=read_Mnist_Label("MNIST/t10k-labels.idx1-ubyte");
-	cout << mnist_label_data.rows << " :" << mnist_label_data.cols << endl;
-	//writeMatToFile(mnist_label_data,"mnist_label_data.txt");
+	Mat mnist_label_data=read_Mnist_Label("MNIST/train-labels.idx1-ubyte");
+	cout << mnist_label_data.rows << " :" << mnist_label_data.cols << endl;*/
 
-	Mat layerSizes = (Mat_<int>(1,3) << 784,100,10);
-	bp.create(layerSizes, CvANN_MLP::SIGMOID_SYM, 0.6, 1);
+//	writeMatToFile(mnist_image_data, "mnist_image_data.txt");
+//	writeMatToFile(mnist_label_data,"mnist_label_data.txt");
+
+/*	Mat layerSizes = (Mat_<int>(1,3) << 784,100,10);
+	cout << layerSizes;
+	bp.create(layerSizes, CvANN_MLP::SIGMOID_SYM, 1, 1);
 	
-
+	cout << "train..." << endl;
 	bp.train(mnist_image_data, mnist_label_data, Mat(), Mat(), params);
-	
+	cout << "end" << endl;
+	*/
+	/*Mat mnist_image_data = read_mnist_image("MNIST/t10k-images.idx3-ubyte");
+	cout << mnist_image_data.rows << " :" << mnist_image_data.cols << endl;
+
+	Mat mnist_label_data = read_Mnist_Label("MNIST/t10k-labels.idx1-ubyte");
+	cout << mnist_label_data.rows << " :" << mnist_label_data.cols << endl;*/
+	CvANN_MLP bp;
+	bp.load("bp.xml");
+
+	float sample[1][784] = { 
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.329412, 0.72549, 0.623529, 0.592157, 0.235294, 0.141176, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.870588, 0.996078, 0.996078, 0.996078, 0.996078, 0.945098, 0.776471, 0.776471, 0.776471, 0.776471, 0.776471, 0.776471, 0.776471, 0.776471, 0.666667, 0.203922, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.262745, 0.447059, 0.282353, 0.447059, 0.639216, 0.890196, 0.996078, 0.882353, 0.996078, 0.996078, 0.996078, 0.980392, 0.898039, 0.996078, 0.996078, 0.54902, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.0666667, 0.258824, 0.054902, 0.262745, 0.262745, 0.262745, 0.231373, 0.0823529, 0.92549, 0.996078, 0.415686, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.32549, 0.992157, 0.819608, 0.07 } };
+	Mat sampleMat(1, 784, CV_32FC1, sample);
+	Mat responseMat;
+
+	bp.predict(sampleMat, responseMat);
+	float* p = responseMat.ptr<float>(0);
+	cout << responseMat << endl;
+	cout<< p <<endl;
+
 	/*int width = 512, height = 512;
 	Mat image = Mat::zeros(height, width, CV_8UC3);
 	Vec3b green(0, 255, 0), blue(255, 0, 0);
@@ -175,8 +211,8 @@ int main(){
 	circle(image, Point(500, 500), 5, Scalar(255, 255, 255), thickness, lineType);	
 	imshow("BP", image); */
 	
-	bp.save("bp.xml");
-
+	
+	
 	waitKey(0);
 	while (true){if (waitKey(10) == 27)break;
 	}
